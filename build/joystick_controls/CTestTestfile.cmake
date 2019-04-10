d13fba353e41150a5228b46aa43af9d477aeaac9
poparrish <parkerparrish@u.boisestate.edu>
2019-04-10 16:59:14 -0600

    added launch file

---
+++ build/joystick_controls/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bender/Downloads/BenderVectorControl-master/src/joystick_controls
+# Build directory: /home/bender/Downloads/BenderVectorControl-master/build/joystick_controls
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
